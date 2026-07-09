f11854452763bd3a842e2331df59bb39b049acfe
Farzan Khan <farzankhan1800@gmail.com>
2026-07-10 01:28:14 +0530

    express setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => res.json({ ok: true }))
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('mongo connected')
+    app.listen(process.env.PORT, () =>
+      console.log(`server on ${process.env.PORT}`))
+  })
+  .catch(err => console.error('mongo error', err))
